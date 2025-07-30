380089131b151df6817f9a487ab0f1f18655b6bd
meghanaM0211 <kasinathvaradi@gmail.com>
2025-07-30 13:48:23 +0530

    adding all files

---
+++ app.py
@@ -0,0 +1,12 @@
+name="MLWE"
+number=10.0
+isOn=True
+letter="k"
+#Airthematic=+ - * /%
+#logical - and,or,not
+#comparision -<, > , ==, !=,<=,>=
+#assignment - =,+=,-=,*=,/=
+if (number>5):
+   print(name) 
+else:
+    print(isOn)
---
+++ app2.py
@@ -0,0 +1,7 @@
+number=9
+if (number%2==0):
+    print("the number is even number")
+else:
+    print("the number is odd number")
+
+
